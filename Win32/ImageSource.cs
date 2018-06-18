e66e7d186285981288ac9edacf60fd98dfd59c43
Mladen Stanišić <lordstanius@gmail.com>
2018-06-18 23:15:28 +0200

    Refactored.

--- Win32/ImageSource.cs
+++ Win32/ImageSource.cs
@@ -10,10 +10,9 @@ namespace Digger.Win32
         public Color[] palette;
 
         private readonly byte[] pixels;
-        private readonly int[] pix;
         private readonly int[] pixs;
-        private readonly int w;
-        private readonly int h;
+        private readonly int width;
+        private readonly int height;
         private readonly int scalef;
         private readonly Bitmap bmp1;
         private readonly Bitmap bmp2;
@@ -24,58 +23,52 @@ namespace Digger.Win32
         public ImageSource(int w, int h, Color[] palette, byte[] pixels, int scalef)
         {
             this.pixels = pixels;
-            this.w = w * scalef;
-            this.h = h * scalef;
+            this.width = w * scalef;
+            this.height = h * scalef;
             this.scalef = scalef;
 
             this.palette = palette;
-            pix = new int[w * h];
-            pixs = new int[this.w * this.h]; // scaled 2x
-            bmp1 = new Bitmap(this.w, this.h, PixelFormat.Format32bppArgb);
-            bmp2 = new Bitmap(this.w, this.h, PixelFormat.Format32bppArgb);
+            pixs = new int[width * height];
+            bmp1 = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            bmp2 = new Bitmap(width, height, PixelFormat.Format32bppArgb);
             bmp0 = bmp1;
         }
 
         public void UpdateImage()
         {
-            for (int i = 0; i < pix.Length; i++)
+            int size = (width * height) / (2 * scalef);
+            for (int i = 0; i < size; i++)
                 Map(i, palette[pixels[i]].ToArgb());
 
-            try
-            {
-                BitmapData bmpData = bmp1.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-                Marshal.Copy(pixs, 0, bmpData.Scan0, w * h);
-                bmp1.UnlockBits(bmpData);
-                bmp0 = bmp1;
-            }
-            catch
-            {
-                BitmapData bmpData = bmp2.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-                Marshal.Copy(pixs, 0, bmpData.Scan0, w * h);
-                bmp2.UnlockBits(bmpData);
-                bmp0 = bmp2;
-            }
+            try { bmp0 = UpdateBitmap(bmp1); }
+            catch { bmp0 = UpdateBitmap(bmp2); }
+        }
+
+        public Bitmap UpdateBitmap(Bitmap bmp)
+        {
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(pixs, 0, bmpData.Scan0, width * height);
+            bmp.UnlockBits(bmpData);
+
+            return bmp;
+        }
+
+        private void Map(int index, int color)
+        {
+            int x = (index % (width / scalef)) * scalef;
+            int y = (index / (width / scalef)) * scalef;
+
+            for (int i = 0; i < scalef; ++i)
+                for (int j = 0; j < scalef; ++j)
+                {
+                    index = (x + i) + (y + j) * width;
+                    pixs[index] = color;
+                }
         }
 
         public void Render(Graphics gfx)
         {
             gfx.DrawImage(bmp0, 0, 0);
-        }
-
-        private void Map(int index, int color)
-        {
-            int x = (index % (w / scalef)) * scalef;
-            int y = (index / (w / scalef)) * scalef;
-            int c = pix[index] = color;
-
-            for (int i = 0; i < scalef; ++i)
-                for (int j = 0; j < scalef; ++j)
-                    pixs[GetIndex(x + i, y + j)] = c;
-        }
-
-        private int GetIndex(int x, int y)
-        {
-            return x + y * w;
         }
     }
 }
